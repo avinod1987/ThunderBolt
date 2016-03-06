dcc05ec7c4c571dcdaf26a42904e46e822f6e72f
sangonsalves <santosh.gonsalves@liradolf.com>
2016-03-07 01:38:50 +0530

    Added Web Project

---
+++ Thunderbolt/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Thunderbolt.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Vinod";
+            return View();
+        }
+        public ActionResult Daily()
+        {
+            return PartialView();
+        }
+        public ActionResult Charts()
+        {
+            return PartialView();
+        }
+        public ActionResult TimeLine()
+        {
+            return PartialView();
+        }
+    }
+}
